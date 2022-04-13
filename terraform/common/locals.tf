01c69a855eeb659d822b4e74ce3362c745477ac1
Your Name <hudsasharma10@gmail.com>
2022-04-13 15:34:12 +0530

    static

--- terraform/common/locals.tf
+++ terraform/common/locals.tf
@@ -3,6 +3,7 @@ locals {
   name = "react-${var.env}"
   application_name = "odw-${var.env}-app"
   location = "eastus2"
+  source = "https://github.com/18-ashish-sharma/terrafrom_react"
   
   tags = {
       env         = var.env,
