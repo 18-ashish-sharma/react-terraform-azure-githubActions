d5ad4a4d1a9d647599a683c63c52889529465ef1
Your Name <hudsasharma10@gmail.com>
2022-04-13 15:04:32 +0530

    static

--- terraform/common/resource.tf
+++ terraform/common/resource.tf
@@ -7,7 +7,6 @@ resource "azurerm_static_site" "static-app" {
   name = "web-${local.name}-np-${local.location}"
   resource_group_name = azurerm_resource_group.react-rg.name
   location = local.location
-  tags = local.common_tags
 }
 
 
