79d1253878c67aa86a8bb2bbbb3b70643b43de16
Your Name <hudsasharma10@gmail.com>
2022-04-13 15:29:37 +0530

    static

--- terraform/common/resource.tf
+++ terraform/common/resource.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "react-rg" {
-  name = "rg-${local.name}-np-${local.location}"
+  name = "react-${local.name}-np-${local.location}"
   location = local.location
 }
 
@@ -7,6 +7,10 @@ resource "azurerm_static_site" "static-app" {
   name = "web-${local.name}-np-${local.location}"
   resource_group_name = azurerm_resource_group.react-rg.name
   location = local.location
+  source = local.source
+  branch = "main"
+  app-location = "/"
+  output-location = "build" 
 }
 
 
