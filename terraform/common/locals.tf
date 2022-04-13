f55bc3221f77467d76791affe6ca017db512e8b5
Ashish Sharma <74248496+18-ashish-sharma@users.noreply.github.com>
2022-04-13 15:07:50 +0530

    Update locals.tf

--- terraform/common/locals.tf
+++ terraform/common/locals.tf
@@ -2,7 +2,7 @@ locals {
   resource_group = "odw-frontend-${var.env}"
   name = "react-${var.env}"
   application_name = "odw-${var.env}-app"
-  location = "centralindia"
+  location = "eastus2"
   
   tags = {
       env         = var.env,
