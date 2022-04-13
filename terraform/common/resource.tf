fe0035b3e5534e40a31218019b00443c03a592d0
Your Name <hudsasharma10@gmail.com>
2022-04-13 14:58:33 +0530

    static

--- terraform/common/resource.tf
+++ terraform/common/resource.tf
@@ -1,14 +1,14 @@
-# resource "azurerm_resource_group" "react-rg" {
-#   name = "rg-${local.name}-np-${local.location}"
-#   location = local.location
-# }
+resource "azurerm_resource_group" "react-rg" {
+  name = "rg-${local.name}-np-${local.location}"
+  location = local.location
+}
 
-# # resource "azurerm_static_site" "static-app" {
-# #   name = "web-${local.name}-np-${local.location}"
-# #   resource_group_name = azurerm_resource_group.react-rg.name
-# #   location = local.location
-# #   tags = local.common_tags
-# # }
+# resource "azurerm_static_site" "static-app" {
+#   name = "web-${local.name}-np-${local.location}"
+#   resource_group_name = azurerm_resource_group.react-rg.name
+#   location = local.location
+#   tags = local.common_tags
+# }
 
 
 # # Generate a random integer to create a globally unique name
@@ -41,47 +41,47 @@
 #   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 # }
 
-resource "random_string" "unique" {
-  length  = 4
-  lower   = true
-  number  = true
-  upper   = false
-  special = false
-}
+# resource "random_string" "unique" {
+#   length  = 4
+#   lower   = true
+#   number  = true
+#   upper   = false
+#   special = false
+# }
 
-resource "azurerm_resource_group" "linux" {
-  name     = "rg-linux-${random_string.unique.result}"
-  location = "centralus"
-}
+# resource "azurerm_resource_group" "linux" {
+#   name     = "rg-linux-${random_string.unique.result}"
+#   location = "centralus"
+# }
 
-resource "azurerm_app_service_plan" "linux" {
-  name                = "plan-linux-${random_string.unique.result}"
-  location            = azurerm_resource_group.linux.location
-  resource_group_name = azurerm_resource_group.linux.name
-  kind                = "Linux"
-  reserved            = true # Must be true for Linux plans
+# resource "azurerm_app_service_plan" "linux" {
+#   name                = "plan-linux-${random_string.unique.result}"
+#   location            = azurerm_resource_group.linux.location
+#   resource_group_name = azurerm_resource_group.linux.name
+#   kind                = "Linux"
+#   reserved            = true # Must be true for Linux plans
 
-  sku {
-    tier     = "Free"
-    size     = "F1"
-    capacity = 1 # Must be at least 1 when reserved is true
-  }
-}
+#   sku {
+#     tier     = "Free"
+#     size     = "F1"
+#     capacity = 1 # Must be at least 1 when reserved is true
+#   }
+# }
 
 
-resource "azurerm_app_service" "linux" {
-  name                = "app-linux-${random_string.unique.result}"
-  location            = azurerm_resource_group.linux.location
-  resource_group_name = azurerm_resource_group.linux.name
-  app_service_plan_id = azurerm_app_service_plan.linux.id
+# resource "azurerm_app_service" "linux" {
+#   name                = "app-linux-${random_string.unique.result}"
+#   location            = azurerm_resource_group.linux.location
+#   resource_group_name = azurerm_resource_group.linux.name
+#   app_service_plan_id = azurerm_app_service_plan.linux.id
 
-  site_config {
-    # Free tier only supports 32-bit
-    use_32_bit_worker_process = true
-    # Run "az webapp list-runtimes --linux" for current supported values, but
-    # always connect to the runtime with "az webapp ssh" or output the value
-    # of process.version from a running app because you might not get the
-    # version you expect
-    linux_fx_version = "NODE|16-lts"
-  }
-}
+#   site_config {
+#     # Free tier only supports 32-bit
+#     use_32_bit_worker_process = true
+#     # Run "az webapp list-runtimes --linux" for current supported values, but
+#     # always connect to the runtime with "az webapp ssh" or output the value
+#     # of process.version from a running app because you might not get the
+#     # version you expect
+#     linux_fx_version = "NODE|16-lts"
+#   }
+# }
