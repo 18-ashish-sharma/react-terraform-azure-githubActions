e3532d87814834e5b29f9f3bf7630eb488aa149d
Your Name <hudsasharma10@gmail.com>
2022-04-14 09:23:26 +0530

    change app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Card, Col, Row } from "react-bootstrap";
 
-function App() {
+const HomePage = () => {
+  const [toonInfo, setToonInfo] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        `https://api4all.azurewebsites.net/api/people/`
+      );
+      const body = await result.json();
+      setToonInfo(body);
+    };
+    fetchData();
+  }, []);
+
+  const renderCard = (card, index) => {
+    return (
+      <Col>
+        <Card style={{ width: "50%" }} key={index}>
+          <Card.Img variant="top" src={card.pictureUrl} />
+          <Card.Body>
+            <Card.Title style={{ fontSize: "12px" }}>
+              {card.firstName} {card.lastName}
+            </Card.Title>
+            <Card.Text style={{ fontSize: "10px" }}>
+              {card.occupation}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Row xs={1} sm={2} md={3} lg={5} className="g-4">
+        {Array.from(toonInfo).map((item, key) => renderCard(item, key))}
+      </Row>
+    </React.Fragment>
   );
-}
+};
 
-export default App;
+export default HomePage;
