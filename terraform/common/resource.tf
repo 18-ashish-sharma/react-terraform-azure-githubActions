19fd4011ea717148f6067355cf3278531e202f78
Your Name <hudsasharma10@gmail.com>
2022-04-17 08:31:50 +0530

    CDN CODE

--- terraform/common/resource.tf
+++ terraform/common/resource.tf
@@ -17,4 +17,40 @@ resource "azurerm_storage_container" "react-blob-container" {
   container_access_type = "private"
 }
 
+resource "azurerm_cdn_profile" "react-cdn-profile" {
+  name                = "example-profile"
+  location            = azurerm_resource_group.react-rg.location
+  resource_group_name = azurerm_resource_group.react-rg.name
+  sku                 = "Standard_Verizon"
+}
 
+resource "azurerm_cdn_endpoint" "react-cdn-endpoint" {
+  name                = "example-endpoint"
+  profile_name        = azurerm_cdn_profile.react-cdn-profile.name
+  location            = azurerm_resource_group.react-rg.location
+  resource_group_name = azurerm_resource_group.react-rg.name
+
+  origin {
+    name      = "onedoorway"
+    host_name = azurerm_storage_account.react-storage-account.primary_blob_host
+  }
+}
+
+data "azurerm_dns_zone" "react-dns-zone" {
+  name                = "example-domain.com"
+  resource_group_name = azurerm_resource_group.react-rg.name
+}
+
+resource "azurerm_dns_cname_record" "react-dns-cname-record" {
+  name                = "react-onedoorway"
+  zone_name           = data.azurerm_dns_zone.react-dns-zone.name
+  resource_group_name = azurerm_resource_group.react-rg.name
+  ttl                 = 3600
+  target_resource_id  = azurerm_cdn_endpoint.react-cdn-endpoint.id
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "react-custom-domain" {
+  name            = "react-onedoorway-domain"
+  cdn_endpoint_id = azurerm_cdn_endpoint.react-cdn-endpoint.id
+  host_name       = "${azurerm_dns_cname_record.react-dns-cname-record.name}.${data.azurerm_dns_zone.react-dns-zone.name}"
+}
